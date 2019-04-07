3f8c6ab4f3b937880a1e959074ac03f5b5bfb987
Aaron <tinn.3@buckeyemail.osu.edu>
2019-04-06 19:13:14 -0500

    saving

---
+++ hw1/README.txt
@@ -0,0 +1,66 @@
+---------------------------------------------------------------------------------------------------------------------------------------------
+Specs:
+
+    Ubuntu 18.04
+    Python 3.5
+
+---------------------------------------------------------------------------------------------------------------------------------------------
+Running the Python Script:
+
+    python hw1.py
+
+---------------------------------------------------------------------------------------------------------------------------------------------
+Output (Should be similar):
+
+    Instance ID: i-05d90df919c1a9f30
+    Launched at 2019-04-05 22:56:44+00:00 in availability zone us-east-1d
+    This instance has termination protection set...
+    Instance termination Failed
+    The instance i-05d90df919c1a9f30 may not be terminated. Modify its 'disableApiTermination' instance attribute and try again.
+
+    Instance ID: i-0aab6964a2a9d02d8
+    Launched at 2019-04-05 22:59:56+00:00 in availability zone us-east-1d
+    This instance does not have termination protection set...Attempting to set protection...
+    This instance has already been terminated
+    Instance has been Terminated!
+
+    Number of EBS Volumes: 34
+    Total provisioned storage (GB):  272
+
+    Port      Inbound IP Address Ranges(s)
+    ----    -------------------------------
+    5432    ['0.0.0.0/0']
+    4433    ['0.0.0.0/0']
+    5000    ['0.0.0.0/0', '34.229.171.92/30']
+    8089    ['0.0.0.0/0']
+    3306    ['0.0.0.0/0']
+    445     ['98.253.27.0/24']
+    443     ['0.0.0.0/0']
+
+---------------------------------------------------------------------------------------------------------------------------------------------
+Reading the ssh_session.txt:
+
+    cat ssh_session.txt
+
+---------------------------------------------------------------------------------------------------------------------------------------------
+Comments:
+
+    - Everything seems to be working okay.
+
+    - I have left one instance running on 'stopped' status for testing, Also I terminated an instance and ran along side the stopped
+    instance which you can see above (i-0aab6964a2a9d02d8)
+
+    - This script also assumes I am logged into the aws and I do not need to provide any further credentials
+
+    - Reading the ssh_session.txt file in a text editor has alot of strange symbols when generating the txt file using:
+
+        script ssh_session.txt
+
+     thus it is best to view the document via the terminal by using cat command.
+
+    - There should be 4 commands seen in the ssh_session.txt. Just in case I will paste them below:
+
+        aws ec2 run-instances --image-id ami-0ce8936181a9b7073 --count 1 --instance-type t2.nano --key-name atinn --security-groups mpcs
+        aws ec2 create-tags --resources i-05d90df919c1a9f30 --tags Key=Name,Value=atinn
+        aws ec2 run-instances --image-id ami-0ce8936181a9b7073 --count 1 --instance-type t2.nano --key-name atinn --security-groups mpcs --tag-specifications 'Resourcepe=instance,Tags=[{Key=Name,Value=atinn}]'
+        ssh -i ~/.ssh/atinn.pem ubuntu@ec2-54-92-216-69.compute-1ec2-52-71-254-61.compute-1.amazonaws.com
--- hw1/hw1.py
+++ hw1/hw1.py
@@ -27,37 +27,54 @@ def instance_protect(name):
                 attribute = ec2client.describe_instance_attribute(Attribute='disableApiTermination', InstanceId=instance['InstanceId'])
 
                 # if False, set termination protection
-                if attribute['DisableApiTermination']['Value'] == False:
-                    print('This instance does not have termination protection set...Attempting to set protection...')
-                    ec2client.modify_instance_attribute(DisableApiTermination={'Value':True}, InstanceId=instance['InstanceId'])
-                    print('This instance now has termination protection set...')
-                else:
-                    print('This instance has termination protection set...')
+                try:
+                    if attribute['DisableApiTermination']['Value'] == False:
+                        print('This instance does not have termination protection set...Attempting to set protection...')
+                        ec2client.modify_instance_attribute(DisableApiTermination={'Value':True}, InstanceId=instance['InstanceId'])
+                        print('This instance now has termination protection set...')
+                    else:
+                        print('This instance has termination protection set...')
+                # If the instance has already been terminated but still in instance list
+                except botocore.exceptions.ClientError:
+                    print('This instance has already been terminated')
 
                 # attempt to terminate
+                # termination successful
                 try:
                     ec2client.terminate_instances(InstanceIds=[instance['InstanceId']])
                     print('Instance has been Terminated!')
+
+                # termination unsuccessful
                 except botocore.exceptions.ClientError:
                     print('Instance termination Failed')
                     print('The instance {} may not be terminated. Modify its \'disableApiTermination\' instance attribute and try again.'.format(instance['InstanceId']))
                 print()
 
 def ebs_profile():
-    ec2client = boto3.resource('ec2')
-    volumes = [volume.size for volume in ec2client.volumes.all()]
+
+    # get the size of each volume and put into array
+    ec2resource = boto3.resource('ec2')
+    volumes = [volume.size for volume in ec2resource.volumes.all()]
+
+    # print count
     print('Number of EBS Volumes:', len(volumes))
+
+    # print sum
     print('Total provisioned storage (GB): ', sum(volumes))
     print()
 
 def security_group_rules():
+
     ec2client = boto3.client('ec2')
 
+    # retrieve the IP information
     response = ec2client.describe_security_groups(GroupNames=['hwdemo'])['SecurityGroups'][0]['IpPermissions']
-    print('Port    Inbound IP Address Ranges(s)')
-    print('----  -------------------------------')
+    print('Port      Inbound IP Address Ranges(s)')
+    print('----    -------------------------------')
+
+    #print each port
     for i in response:
-        print(i['FromPort'], end='   ')
+        print(i['FromPort'], end='    ')
         print([x['CidrIp'] for x in i['IpRanges']])
 
 def main(name):
